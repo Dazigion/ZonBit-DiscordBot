7230615ec86e81d93cf939b9089faf1fa50a14d5
Dazigion <82140291+Dazigion@users.noreply.github.com>
2022-04-15 16:35:48 +0800

    first files i guess
    i have no clue on how to use github

---
+++ Database.js
@@ -0,0 +1,49 @@
+const fs = require("fs")
+const StorageLocation = "./Storage"
+
+let rawfunc = {
+		
+	save:(id,idType,data) => {
+		return new Promise((res,rej)=>{
+			let location = StorageLocation + "/" + idType + "/"
+			data = JSON.stringify(data)
+			
+			if (!fs.existsSync(location)) fs.mkdirSync(location)
+			
+			fs.writeFile(location + id ,data, function(err) {
+			if (err) rej(err) 
+			res(true)
+			})
+		})
+	},
+	
+	load: (id,idType) => {
+		return new Promise ((res,rej)=>{
+			let location = StorageLocation + "/" + idType + "/"
+			
+			if (!fs.existsSync(location + id)) rej("NoFileSelected")
+			
+			fs.readFile(location + id,(err,data)=>{
+				if (err) rej(err)
+				res(JSON.parse(data))
+			})
+			
+		})
+	},
+	
+	update: (id,idType,data) => {
+		return new Promise (async (res,rej)=>{
+			let OldData = await rawfunc.load(id,idType)
+			await rawfunc.save(id,idType,{...OldData,...data})
+			res(true)	
+		})
+	},
+	
+	delete: (id,idType) => {
+		fs.unlink()
+	}
+	
+}
+
+
+module.exports = rawfunc
